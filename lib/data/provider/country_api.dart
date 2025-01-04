6b169dc29c5003f36bfa6853b238268ff1cf345f
nyamajiwa-blessing <bnyamajiwa00@gmail.com>
2025-01-05 00:53:29 +0200

    Exception handling

--- lib/data/provider/country_api.dart
+++ lib/data/provider/country_api.dart
@@ -1,16 +1,25 @@
 import 'dart:convert';
+// import 'dart:math';
 import 'package:http/http.dart' as http;
 
 import 'package:countries_restapi/data/models/country.dart';
 
 class CountryApi {
-  Future<List<Country>?> getAllCountries() async{
-    var client = http.Client();
-    var uri = Uri.parse('https://restcountries.com/v3.1/all');
-    var response = await client.get(uri);
-    if (response.statusCode == 200) {
-      return countryFromJson(const Utf8Decoder().convert(response.bodyBytes));
+  Future<List<Country>?> getAllCountries() async {
+    final url = Uri.parse('https://restcountries.com/v3.1/all');
+    try {
+      final response = await http.get(url);
+      if (response.statusCode == 200) {
+        return json.decode(response.body);
+      } else {
+        throw Exception('Failed to load countries');
+      }
+    } on http.ClientException catch (e) {
+      print('CleintException $e');
+      throw Exception('Failed to load countries');
+    } catch (e) {
+      print('Exception $e');
+      throw Exception('Failed to load countries');
     }
-    return null;
   }
 }
